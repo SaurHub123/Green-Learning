51f23e18576207683c74fc3d8bbe73472985d7a8
kajalrituraj <kajalrituraj2002@gmail.com>
2024-01-12 00:18:52 +0530

    reusable components

--- lib/views/one_day_access/dashboard/dashboard_screen.dart
+++ lib/views/one_day_access/dashboard/dashboard_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:green_learning/views/common/components/custom_cards.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../../../utils/constants.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -14,11 +17,41 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          "Dashboard",
-          style: GoogleFonts.rajdhani(
-            color: Colors.black,
-            fontSize: 23.sp,
+        child: Container(
+          height: 100.h,
+          width: 100.w,
+          padding: EdgeInsets.symmetric(horizontal: 2.5.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: CustomCards.smallIconTextCard("Account", Icons.school,
+                    Colors.red, Constants.primaryColor, null, null),
+              ),
+              Row(
+                children: [
+                  CustomCards.curvedCard(
+                    Colors.yellow,
+                    Colors.pink,
+                    Icons.ac_unit_rounded,
+                    Constants.primaryColor,
+                    "2000+",
+                    "Mentors",
+                    true,
+                  ),
+                  SizedBox(width: 2.5.w),
+                  CustomCards.curvedCard(
+                    Colors.yellow,
+                    Colors.pink,
+                    Icons.ac_unit_rounded,
+                    Constants.primaryColor,
+                    "2000+",
+                    "Mentors",
+                    false,
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
