ad5f6cb3b4890b814d5c533f0183e4fbbc89c830
kajalrituraj <kajalrituraj2002@gmail.com>
2024-01-11 22:11:16 +0530

    added about us screen..

--- lib/views/one_day_access/account/account_screen.dart
+++ lib/views/one_day_access/account/account_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:green_learning/utils/constants.dart';
+import 'package:green_learning/views/one_day_access/account/profile_menu.dart';
+import 'package:green_learning/views/one_day_access/account/update_profile_screen.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:get/get.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -13,21 +16,126 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Constants.backgroundColor,
-      body: SafeArea(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () => Get.back(), icon: const Icon(Icons.arrow_back)),
+        title:
+            Text("Profile", style: Theme.of(context).textTheme.headlineMedium),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.sunny))
+        ],
+      ),
+      body: SingleChildScrollView(
         child: Container(
-          height: 100.h,
-          width: 100.w,
-          padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.w),
-          child: const SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Account Screen"),
-              ],
-            ),
+          padding: EdgeInsets.all(4.w),
+          child: Column(
+            children: [
+              /// -- IMAGE
+              Stack(
+                children: [
+                  Container(
+                    width: 130,
+                    height: 130,
+                    padding: EdgeInsets.all(8.sp),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: Constants.primaryColor, width: 6.sp),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(3.w),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/avt1.png")),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Constants.primaryColor),
+                      child: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text("Kajal Rituraj",
+                  style: Theme.of(context).textTheme.headline4),
+              Text("kajalrituraj2002@gmail.com",
+                  style: Theme.of(context).textTheme.bodyText2),
+              const SizedBox(height: 20),
+
+              /// -- BUTTON
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () => Get.to(() => const UpdateProfileScreen()),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Constants.primaryColor,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: const Text("EditProfile",
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Divider(),
+              const SizedBox(height: 10),
+
+              /// -- MENU
+              ProfileMenuWidget(
+                  title: "Settings", icon: Icons.settings, onPress: () {}),
+              ProfileMenuWidget(
+                  title: "Billing Details", icon: Icons.wallet, onPress: () {}),
+              ProfileMenuWidget(
+                  title: "User Management",
+                  icon: Icons.verified_user,
+                  onPress: () {}),
+              const Divider(),
+              const SizedBox(height: 10),
+              ProfileMenuWidget(
+                  title: "Information", icon: Icons.info, onPress: () {}),
+              ProfileMenuWidget(
+                  title: "Logout",
+                  icon: Icons.logout,
+                  textColor: Colors.red,
+                  endIcon: false,
+                  onPress: () {
+                    Get.defaultDialog(
+                      title: "LOGOUT",
+                      titleStyle: const TextStyle(fontSize: 20),
+                      content: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        child: Text("Are you sure, you want to Logout?"),
+                      ),
+                      confirm: Expanded(
+                        child: ElevatedButton(
+                          // onPressed: () => AuthenticationRepository.instance.logout(),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xffB5D9FF),
+                              side: BorderSide.none),
+                          onPressed: () {},
+                          child: const Text("Yes"),
+                        ),
+                      ),
+                      cancel: OutlinedButton(
+                          onPressed: () => Get.back(), child: const Text("No")),
+                    );
+                  }),
+            ],
           ),
         ),
       ),
